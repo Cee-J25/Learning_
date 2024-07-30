b2c4702a6acdd105d5739c07d5e864b45dafa58d
Lindor <radebelindo14@gmail.com>
2024-07-30 09:34:32 +0200

    Update Jul_30.java

--- Jul_30.java
+++ Jul_30.java
@@ -1,1 +1,12 @@
+//@author Cee-J 
+public class Jul_30 {
 
+  public static void main(String[] args) {
+    System.out.println("Let there be Light");
+    for (int i = 0; i < 11; i++) {
+      System.out.println(".");
+
+    System.out.println("And there was light");
+    }
+  }
+}
