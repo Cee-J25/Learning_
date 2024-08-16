f48eb9f8f4e5e1174ffb0a93e828c3ab2f6ace87
Lindor <radebelindo14@gmail.com>
2024-08-16 10:41:19 +0200

    Update Jul_30.java

--- Jul_30.java
+++ Jul_30.java
@@ -7,6 +7,14 @@ public class Jul_30 {
       System.out.println(".");
 
     System.out.println("And there was light");
+
+
+      for (int i = 0; i <= 5; i++) {
+        for (int j <= i; j++) {
+          System.out.print("* " + " ");
+        }
+        System.out.println("")
+      }
     }
   }
 }
